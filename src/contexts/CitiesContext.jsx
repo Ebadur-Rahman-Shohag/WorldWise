22a9c9bed02033593cb6ff2db9e7b8496b116b4f
Ebadur Rahman <ebadurshohag@gmail.com>
2023-12-24 15:27:48 +0600

    update data

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,7 +6,7 @@ import {
   useCallback,
 } from "react";
 
-const BASE_URL = "http://localhost:9000";
+const BASE_URL = "https://worldwiseserver-wqjq.onrender.com";
 
 const CitiesContext = createContext();
 
